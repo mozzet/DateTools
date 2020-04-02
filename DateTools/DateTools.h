7d2c019387e136bf9a6b7776e8b0e6c41655e073
Jo Sesang <ss.cho@mozzet.com>
2020-04-02 17:28:30 +0900

    카르타고 지원하도록 변경

---
+++ DateTools/DateTools.h
@@ -0,0 +1,19 @@
+//
+//  DateTools.h
+//  DateTools
+//
+//  Created by MOZZET PC on 2020/04/02.
+//  Copyright © 2020 Noondate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateTools.
+FOUNDATION_EXPORT double DateToolsVersionNumber;
+
+//! Project version string for DateTools.
+FOUNDATION_EXPORT const unsigned char DateToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateTools/PublicHeader.h>
+
+
